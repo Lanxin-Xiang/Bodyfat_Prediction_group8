93f91ccfe1edf2cae2a2d263b3dde9467a46d8d5
Lanxin-Xiang <91483229+Lanxin-Xiang@users.noreply.github.com>
2021-10-20 15:15:48 -0500

    Update modeling and analysis.R

--- Code/modeling and analysis.R
+++ Code/modeling and analysis.R
@@ -11,10 +11,9 @@ a <- sum(simp_m$residuals^2)
 
 
 step(lm(BODYFAT~.,data = ddf[,c(-1,-3,-4,-7)]))
-full_m <-lm(BODYFAT ~ HEIGHT + NECK + CHEST + ABDOMEN + 
-              FOREARM + WRIST + Myage,data = ddf) 
+full_m <-lm(BODYFAT ~ HEIGHT + NECK + CHEST + ABDOMEN + FOREARM + WRIST + Myage,data = ddf) 
 summary(full_m)
-b <- sqrt(sum(full_m$residuals^2))/nrow(ddf)
+b <- sqrt(sum(full_m$residuals^2)/nrow(ddf))
 
 
 ############################## Myage=0 #################################
@@ -23,12 +22,12 @@ b <- sqrt(sum(full_m$residuals^2))/nrow(ddf)
 sub0 <- subset(ddf,ddf$Myage==0)
 simp_m0 <- lm(BODYFAT~WEIGHT+HEIGHT+ABDOMEN,data = sub0)
 summary(simp_m0)
-a1 <- sqrt(sum(simp_m0$residuals^2))/nrwo(sub0)
+a1 <- sqrt(sum(simp_m0$residuals^2)/nrwo(sub0))
 
 step(lm(BODYFAT~.,data = sub0[,c(-1,-3,-4,-7,-18)]))
 full_m0 <-lm(BODYFAT ~ HEIGHT + NECK + ABDOMEN + BICEPS + WRIST,data = sub0) 
 summary(full_m0)
-b1 <- sqrt(sum(full_m0$residuals^2))/nrwo(sub0)
+b1 <- sqrt(sum(full_m0$residuals^2)/nrwo(sub0))
 
 
 ############################# Myage=1 ##################################
@@ -36,14 +35,13 @@ b1 <- sqrt(sum(full_m0$residuals^2))/nrwo(sub0)
 sub1 <- subset(ddf,ddf$Myage==1)
 simp_m1 <- lm(BODYFAT~WEIGHT+HEIGHT+ABDOMEN,data = sub1)
 summary(simp_m1)
-a2 <- sqrt(sum(simp_m1$residuals^2))/nrow(sub1)
+a2 <- sqrt(sum(simp_m1$residuals^2)/nrow(sub1))
 
 
 step(lm(BODYFAT~.,data = sub1[,c(-1,-3,-4,-7,-18)]))
-full_m1 <-lm(BODYFAT ~ ABDOMEN + THIGH + ANKLE + FOREARM + WRIST, 
-             data = sub1)
+full_m1 <-lm(BODYFAT ~ ABDOMEN + THIGH + ANKLE + FOREARM + WRIST, data = sub1)
 summary(full_m1)
-b2 <- sqrt(sum(full_m1$residuals^2))/nrow(sub1)
+b2 <- sqrt(sum(full_m1$residuals^2)/nrow(sub1))
 
 
 ############################## Myage=2 #################################
@@ -51,11 +49,11 @@ b2 <- sqrt(sum(full_m1$residuals^2))/nrow(sub1)
 sub2 <- subset(ddf,ddf$Myage==2)
 simp_m2 <- lm(BODYFAT~WEIGHT+HEIGHT+ABDOMEN,data = sub2)
 summary(simp_m2)
-a3 <- sqrt(sum(simp_m2$residuals^2))/nrow(sub2)
+a3 <- sqrt(sum(simp_m2$residuals^2)/nrow(sub2))
 
 step(lm(BODYFAT~.,data = sub2[,c(-1,-3,-4,-7,-18)]))
-full_m2 <-lm(BODYFAT ~ WEIGHT + NECK + ABDOMEN + ANKLE+BICEPS-FOREARM, data = sub2)
+full_m2 <-lm(BODYFAT ~ WEIGHT + NECK + ABDOMEN + ANKLE+BICEPS+FOREARM, data = sub2)
 summary(full_m2)
-b3 <- sqrt(sum(full_m2$residuals^2))/nrow(sub2)
+b3 <- sqrt(sum(full_m2$residuals^2)/nrow(sub2))
 plot(full_m2)
 
